a74446d3fd5373e233a2165878764fce08ec8c72
Andrew <aglenyoung@gmail.com>
2015-08-06 18:43:47 -0400

    Check to see if the commit is a pull request

--- .drone/build.sh
+++ .drone/build.sh
@@ -6,10 +6,12 @@ set -e
 env | sort
 
 DRONE_PRIVATE_REPO=${DRONE_PRIVATE_REPO:-"false"}
+DRONE_PR=${DRONE_PR:-""}
 
 cd ${DRONE_BUILD_DIR}
 
-if [ "${DRONE_PRIVATE_REPO}" == "false" ]; then
+# Exit early with an error if this is a pull request and the repo isn't private
+if [ "${DRONE_PR}" != "" ] && [ "${DRONE_PRIVATE_REPO}" == "false" ]; then
     echo "Repo is public. Drone cannot run this container in privileged mode."
     exit 1
 fi
