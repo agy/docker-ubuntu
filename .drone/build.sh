b575b394430cd8f46200c65907458b3d07de00ab
Andrew <aglenyoung@gmail.com>
2015-08-06 18:41:37 -0400

    Check to see if the repo is private

--- .drone/build.sh
+++ .drone/build.sh
@@ -5,7 +5,14 @@ set -e
 
 env | sort
 
+DRONE_PRIVATE_REPO=${DRONE_PRIVATE_REPO:-"false"}
+
 cd ${DRONE_BUILD_DIR}
+
+if [ "${DRONE_PRIVATE_REPO}" == "false" ]; then
+    echo "Repo is public. Drone cannot run this container in privileged mode."
+    exit 1
+fi
 
 wrapdocker &
 sleep 5
