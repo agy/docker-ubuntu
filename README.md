0ac03355f05ab7a1dbc8ff19223e486fe731f009
Andrew Glen-Young <agy@xenial>
2016-09-15 17:03:26 -0400

    Update README. Allow ref to be specified

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
-[![Build Status](http://23.253.23.86:8080/api/badge/github.com/agy/docker-ubuntu/status.svg?branch=master)](http://23.253.23.86:8080/github.com/agy/docker-ubuntu)
-
 # docker-ubuntu
 
-A container for testing drone.io
+A container for testing builds
 
-YAY
+## Build process
+
+The build process is a matter of:
+
+ 1. Make changes
+ 2. Push changes to remote repo
+ 3. Tag release and push tags to remote repo
+ 4. Run `make container`
+
+This results in a Docker image built and tagged with:
+
+ 1. The git ref of the build
+ 2. The git tag of the build
+
+## Caveats
+
+ * Only the first git tag is used to tag the Docker image
--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ set -e
 
 CONTAINER=${1:?"Container name required"}
 BRANCH=${2:-"master"}
+REF=${3:-$(git ls-remote --heads origin 2>/dev/null | awk '$2 ~/refs\/heads\/'${BRANCH}'/ { print $1 }')}
 
-REF=$(git ls-remote --heads origin 2>/dev/null | awk '$2 ~/refs\/heads\/'${BRANCH}'/ { print $1 }')
 TAG=$(git tag --points-at ${REF} 2>/dev/null | head --lines 1)
 
 LAST_UPDATED=$(date "+%s")
@@ -36,8 +36,9 @@ image_has_ref() {
 
 main() {
 	if [ -z "${TAG}" ]; then
-		echo "No tags pointing at current ref"
-		echo "Have you pushed the branch to the remote repo?"
+		echo "No tags pointing at ref:"
+		echo " * Have you pushed the branch to the remote repo?"
+		echo " * Have you tagged the ref?"
 		exit 0
 	fi
 
