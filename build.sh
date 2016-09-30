3dec06d9e13f2579fd1ed395ecfca87b334e708b
Andrew Glen-Young <agy@xenial>
2016-09-30 15:14:58 -0400

    Build containers based on tag rather than ref
     * We assume that tags won't be changed, only appended
     * Allows for altering support scripts without needing to build a new
       container

--- build.sh
+++ build.sh
@@ -1,70 +1,116 @@
 #!/bin/bash
+#
+# DESCRIPTION:
+#
+# Builds a container based on the specified git tag. If no tag is
+# passed in we assume that semver tags are used and use the most
+# recent tag.
+#
+# USAGE:
+#
+# $ build.sh mycontainer 1.0.1
+#
 
 set -u
 set -e
 
 CONTAINER=${1:?"Container name required"}
-BRANCH=${2:-"master"}
-GIT_REF=${3:-$(git ls-remote --heads origin 2>/dev/null | awk '$2 ~/refs\/heads\/'${BRANCH}'/ { print $1 }')}
+GIT_TAG=${2:-"latest"}
+GIT_BRANCH=${3:-"master"} # Not yet used - always assumes master
 
-GIT_TAG=$(git tag --points-at ${GIT_REF} 2>/dev/null | head --lines 1)
 GIT_URL=$(git remote get-url origin 2>/dev/null | head --lines 1)
+TMP_DIR=$(mktemp --directory)
 
-LAST_UPDATED=$(TZ="UTC" date --rfc-3339="seconds")
-SUDO="sudo"
+
+trap clean SIGTERM SIGQUIT SIGINT
+
+clean() {
+	rm -rf ${TMP_DIR}
+}
 
 
 # Build a container based on the current remote git ref
 build() {
-	local tmp_dir=$(mktemp --directory)
-	pushd ${tmp_dir}
-	git clone ${GIT_URL} .
+	local container=${1}
+	local git_url=${2}
+	local git_ref=${3}
 
-	${SUDO} docker build \
+	local last_updated=$(TZ="UTC" date --rfc-3339="seconds")
+
+	git clone ${git_url} .
+	sudo docker build \
 		--no-cache \
-		--label="com.datadoghq.build-date"="${LAST_UPDATED}" \
-		--label="com.datadoghq.vcs-url"="${GIT_URL}" \
-		--label="com.datadoghq.vcs-ref"="${GIT_REF}" \
-		--tag ${CONTAINER}:${GIT_REF} \
+		--label="com.datadoghq.build-date"="${last_updated}" \
+		--label="com.datadoghq.vcs-url"="${git_url}" \
+		--label="com.datadoghq.vcs-ref"="${git_ref}" \
+		--tag ${container}:${git_ref} \
 		.
-
-	popd
-	rm -rf ${tmp_dir} || true
 }
 
 # Tag a container based on the current tag pointing at the current git ref
 tag() {
-	${SUDO} docker tag ${CONTAINER}:${GIT_REF} ${CONTAINER}:${GIT_TAG} || true
+	local container=${1}
+	local git_ref=${2}
+	local git_tag=${3}
+
+	sudo docker tag ${container}:${git_ref} ${container}:${git_tag} || true
 }
 
 image_has_ref() {
-	if ${SUDO} docker images --quiet ${CONTAINER}:${GIT_REF} | \
+	local container=${1}
+	local git_ref=${2}
+
+	if sudo docker images --quiet ${container}:${git_ref} | \
 		grep --quiet --extended-regexp '^[0-9a-f]{12}'; then
 		return 0
 	fi
 	return 1
 }
 
+tag_to_ref() {
+	local git_tag=${1}
+
+	echo $(git ls-remote --tags origin 2>/dev/null | \
+		awk '$2 ~/refs\/tags\/'${git_tag}'/ { print $1 }')
+}
+
+tag_name() {
+	local tag=${1}
+
+	case "${tag}" in
+		"latest")
+			# This assumes semver (or similar)
+			git_tag=$(git ls-remote --tags origin 2>/dev/null | \
+				tail --lines 1 | \
+				awk -F/ '{ print $3 }')
+			;;
+		*)
+			git_tag=${tag}
+			;;
+	esac
+
+	echo ${git_tag}
+}
+
 main() {
-	if [ -z "${GIT_TAG}" ]; then
-		echo "No tags pointing at ref:" >&2
-		echo " * Have you pushed the branch to the remote repo?" >&2
-		echo " * Have you tagged the ref?" >&2
+	local git_tag=$(tag_name ${GIT_TAG})
+	local git_ref=$(tag_to_ref ${git_tag})
+
+	if [ -z "${git_ref}" ]; then
+		echo "No remote ref ${git_ref} exists for branch ${GIT_BRANCH}" >&2
 		exit 1
 	fi
 
-	if [ -z "${GIT_REF}" ]; then
-		echo "No remote ref ${GIT_REF} exists for branch ${BRANCH}" >&2
-		exit 1
-	fi
-
-	if image_has_ref; then
-		echo "Image for ref ${GIT_REF} already exists"
+	if image_has_ref ${CONTAINER} ${git_ref}; then
+		echo "Image ${CONTAINER}:${git_ref} already exists"
 		exit 0
 	fi
 
-	build
-	tag
+	pushd ${TMP_DIR}
+	build ${CONTAINER} ${GIT_URL} ${git_ref}
+	popd
+
+	tag ${CONTAINER} ${git_ref} ${git_tag}
 }
 
 main
